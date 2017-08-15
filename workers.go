94235fd635cd69ea4c02589fd02525164888065c
Jeremy Echols <jechols@uoregon.edu>
2017-08-15 11:37:14 -0700

    Add file-copying worker function

--- workers.go
+++ workers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -102,10 +103,46 @@ func (w *Worker) Start() {
 		case XMLFix:
 		case PDFFix:
 		case FileCopy:
-			w.CopyFile()
+			w.CopyFile(j)
 		}
 	}
 }
 
-func (w *Worker) CopyFile() {
+// CopyFile just opens source and copies the contents to the destination path.
+// There is an inordinate amount of error handling because if something goes
+// wrong we *really* need to know exactly what it was.
+func (w *Worker) CopyFile(j *Job) {
+	var in, out *os.File
+	var err error
+
+	in, err = os.Open(j.SourcePath)
+	if err != nil {
+		log.Printf("ERROR: unable to read %q: %s", j.SourcePath, err)
+		return
+	}
+	defer in.Close()
+
+	out, err = os.Create(j.DestPath)
+	if err != nil {
+		log.Printf("ERROR: unable to create %q: %s", j.DestPath, err)
+		return
+	}
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		log.Printf("ERROR: unable to write to %q: %s", j.DestPath, err)
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		log.Printf("ERROR: unable to sync %q: %s", j.DestPath, err)
+		return
+	}
+
+	err = out.Close()
+	if err != nil {
+		log.Printf("ERROR: unable to close %q: %s", j.DestPath, err)
+		return
+	}
 }
